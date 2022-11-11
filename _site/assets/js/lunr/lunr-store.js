3f5581576a88075d4c4982ed2ddbc4d222976c2f
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-11-10 23:45:35 -0500

    updated with photo manager

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,43 +1,49 @@
 var store = [{
-        "title": "Artificial Intelligence: Various Projects",
-        "excerpt":"Project topics included search problems, games, Markov decision processes, reinforcement learning, Bayesian Networks, Markov Models, and machine learning. Tech: Python. Primary libraries include matplotlib, heapq, pgmpy, scipy, numpy. For more information, the course page is here. Results from a Linear Classifier Problem, UCB Bandit Problem, and a Sigmoid Values Problem...","categories": [],
-        "tags": [],
-        "url": "/projects/ai_1/",
-        "teaser": "/assets/images/ai_4.png"
-      },{
         "title": "Cloud Computing: AWS Restaurant Recommending Chatbot",
         "excerpt":"Created a chatbot which uses Yelp data to recommend restaurants to users based on their requirements. It was my first time using AWS. Chatbox is now disabled since running the services cost $, but there’s a video showing functionality below! Github repo Tech: AWS Lambda (Python), IAM, Lex, SQS, DynamoDB,...","categories": [],
         "tags": [],
-        "url": "/projects/cc_1/",
+        "url": "/projects/1cc_1/",
         "teaser": "/assets/images/cc_1.png"
+      },{
+        "title": "Cloud Computing: AWS Photo Manager",
+        "excerpt":"Created a photo album web application that can be searched using AWS’s Transcribe NLP (through both text and voice). Lex disambiguates the query. Query your photos for people, objects, actions, landmarks and more. You can add custom labels to identify your photos with your own indexes. Chatbox is now disabled...","categories": [],
+        "tags": [],
+        "url": "/projects/1cc_2/",
+        "teaser": "/assets/images/cc_4.png"
+      },{
+        "title": "Artificial Intelligence: Various Projects",
+        "excerpt":"Project topics included search problems, games, Markov decision processes, reinforcement learning, Bayesian Networks, Markov Models, and machine learning. Tech: Python. Primary libraries include matplotlib, heapq, pgmpy, scipy, numpy. For more information, the course page is here. Results from a Linear Classifier Problem, UCB Bandit Problem, and a Sigmoid Values Problem...","categories": [],
+        "tags": [],
+        "url": "/projects/2ai_1/",
+        "teaser": "/assets/images/ai_4.png"
       },{
         "title": "Analyst Projects: Domain Upgrades, Reporting Projects, SSRS Conversions",
         "excerpt":"XTO to UPST Domain Upgrade Project was implementing a model for hosting both UPST and XOM users on the application using a system of staging databases and the software that populates each set based on who is leaving/joining the company based on what AD groups they are on. Tasks I...","categories": [],
         "tags": [],
-        "url": "/projects/exxon_1/",
+        "url": "/projects/3exxon_1/",
         "teaser": null
       },{
         "title": "Analyst Projects: Integrations",
         "excerpt":"2021 - Created WV - IPES SAP integration for Argentina division. collaborated with team in Argentina to create outline for their request and built out the entire SQL integration between Wellview and IPES. Managed deployment for users, impacting ~100 wells. 2021 - Created SAP integration USA documentation (spreadsheet, diagrams, and...","categories": [],
         "tags": [],
-        "url": "/projects/exxon_2/",
+        "url": "/projects/4exxon_2/",
         "teaser": null
       },{
         "title": "Greyhound Adoption Website",
         "excerpt":"MIS 374 course teaches concepts and techniques related to the iterative software development lifecycle (SDLC) including planning, analysis, design, build, quality assurance, and implementation. The course also teaches topics such as client management, scope management, and risk management. As a project manager, led a team through a root cause analysis,...","categories": [],
         "tags": [],
-        "url": "/projects/ut_1/",
+        "url": "/projects/5ut_1/",
         "teaser": "/assets/images/ut_1.png"
       },{
         "title": "Women's Interest Network",
         "excerpt":"I was the Site Collection Administrator and sole creator of the new Houston WIN website while I worked at ExxonMobil.   Tech stack: HTML, CSS, Sharepoint, JS, ExxonMobil Unity    \t \tHouston WIN website in 2022.   ","categories": [],
         "tags": [],
-        "url": "/projects/win_1/",
+        "url": "/projects/6win_1/",
         "teaser": "/assets/images/win_1.png"
       },{
         "title": "Grace Hopper Celebration",
         "excerpt":"Designed and created original logo and slogan designs for ExxonMobil at GHC. “Girls fuel the future”. Learned Adobe Illustrator to create 8 prototypes and collaborated with the UX Design team, implementing feedback and finally compiling top designs to present for approval from XOM PR. In addition, created price comparison spreadsheets...","categories": [],
         "tags": [],
-        "url": "/projects/win_2/",
+        "url": "/projects/7win_2/",
         "teaser": "/assets/images/win_2.png"
       }]
